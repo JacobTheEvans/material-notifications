f72c6d88ae94c24732307891de5ecc874135d9e3
jacobtheevans <jakegod14@gmail.com>
2019-02-04 00:35:45 +0100

    Add additional webpack for vanilla js support

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,61 @@
 const path = require('path')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src'),
+module.exports = [
+  {
+    entry: path.resolve(__dirname, 'src'),
 
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'material-notifications',
-    libraryTarget: 'commonjs2'
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'material-notifications',
+      libraryTarget: 'commonjs2'
+    },
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+
+    module: {
+      rules: [{
+        test: /\.(ts|js)x?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react']
+        },
+        exclude: /node_modules/
+      }]
+    },
+
+    plugins: [
+      new ForkTsCheckerWebpackPlugin()
+    ]
   },
+  {
+    entry: path.resolve(__dirname, 'src'),
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
+    output: {
+      filename: 'vanilla-material-notifications.js',
+      path: path.resolve(__dirname, 'dist')
+    },
 
-  module: {
-    rules: [{
-      test: /\.(ts|js)x?$/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', '@babel/react']
-      },
-      exclude: /node_modules/
-    }]
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
 
-  plugins: [
-    new ForkTsCheckerWebpackPlugin()
-  ]
-}
+    module: {
+      rules: [{
+        test: /\.(ts|js)x?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react']
+        },
+        exclude: /node_modules/
+      }]
+    },
+
+    plugins: [
+      new ForkTsCheckerWebpackPlugin()
+    ]
+  }
+]
